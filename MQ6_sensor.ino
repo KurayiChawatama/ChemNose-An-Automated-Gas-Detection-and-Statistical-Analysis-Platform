727b71081cf014bb87752949527878161fcac30b
Kurayi Chawatama <142725139+KurayiChawatama@users.noreply.github.com>
2024-07-04 11:30:47 +0300

    Add files via upload

---
+++ MQ6_sensor.ino
@@ -0,0 +1,21 @@
+const int mq6Pin = A0;
+const int mq2Pin = A1;
+int seconds = 0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int mq6sensorValue = analogRead(mq6Pin);
+  int mq2sensorValue = analogRead(mq2Pin);
+
+  Serial.print(seconds);
+  Serial.print(",");
+  Serial.print(mq6sensorValue);
+  Serial.print(",");
+  Serial.println(mq2sensorValue);
+
+  seconds = seconds + 1;
+  delay(1000);
+}
