e2f4b972a960f31d29fca0b34dc7f5f620e9d69e
Kurayi Chawatama <142725139+KurayiChawatama@users.noreply.github.com>
2024-07-14 00:23:03 +0300

    Update soilMoistureSensor.ino with comments for ease of readablity

--- Arduino_code/soilMoistureSensor.ino
+++ Arduino_code/soilMoistureSensor.ino
@@ -1,14 +1,19 @@
 void setup() {
   Serial.begin(9600);
-  pinMode(A0, INPUT);
+  pinMode(A0, INPUT);   // Set analog pin A0 as input for moisture sensor
 }
 
 void loop() {
-  int rawMois = analogRead(A0);
+  int rawMois = analogRead(A0);  // Read analog input from moisture sensor (0-1023)
+  
+  // Convert raw sensor reading to percentage moisture
   float percMois = 100.0 - (rawMois / 1023.0 * 100.0);
+  
+  // Print moisture percentage to serial monitor
   Serial.print(percMois);
   Serial.print("% - ");
 
+  // Determine soil moisture level based on raw sensor reading
   if (rawMois >= 1000) {
     Serial.println("Sensor is not in the Soil or DISCONNECTED");
   } else if (rawMois < 1000 && rawMois >= 600) {
@@ -19,5 +24,5 @@ void loop() {
     Serial.println("Sensor in WATER");
   }
 
-  delay(2000);
+  delay(2000);  // Delay for 2 seconds before taking next reading
 }
